dbc39221cbb8815d0dc40ebaf3dd8d7bb9444d35
rinpmchs <90576981+rinpmchs@users.noreply.github.com>
2024-06-11 00:12:02 +0300

    update pollard

--- PollardsAlgorithm/pollardAlg.cpp
+++ PollardsAlgorithm/pollardAlg.cpp
@@ -1,5 +1,5 @@
 #include "pollardAlg.h"
-#include "../Useful/algorithms.h"
+#include "../Utils/algorithms.h"
 
 namespace Project::Detail {
 
@@ -7,9 +7,9 @@ LongInteger Pollard::pollard_p_minus_1_fact(const LongInteger& n, const LongInte
     LongInteger m = c;
 
     for (LongInteger i = 1; i < max; i++) {
-        m = Useful::power_mod(m, i , n);
+        m = Utils::power_mod(m, i , n);
         if (i % 10 == 0) {
-            LongInteger g = Useful::gcd(m - 1, n);
+            LongInteger g = Utils::gcd(m - 1, n);
             if (g == n) // we need to change c
                 return 0;
             else if (g > 1) // g is a proper divisor
