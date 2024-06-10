7e316dd6decd9aaf5d87511c3f996cd6b8095feb
rinpmchs <90576981+rinpmchs@users.noreply.github.com>
2024-06-10 20:02:34 +0300

    add integer.h

---
+++ integer.h
@@ -0,0 +1,14 @@
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace Project {
+
+using SmallInteger = int64_t;
+using SmallUInteger = uint64_t;
+using LongInteger = boost::multiprecision::cpp_int;
+
+namespace Detail {}
+namespace Tests {}
+namespace Use {}
+namespace Useful {}
+
+}  // namespace Project
