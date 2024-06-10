3a8c90a345c9501aad818519e69ce39ae09b083f
rinpmchs <90576981+rinpmchs@users.noreply.github.com>
2024-06-10 20:08:58 +0300

    update pollard

--- PollardsAlgorithm/pollardAlg.cpp
+++ PollardsAlgorithm/pollardAlg.cpp
@@ -1,20 +1,15 @@
 #include "pollardAlg.h"
 #include "../Useful/algorithms.h"
 
-namespace Pollard {
+namespace Project::Detail {
 
-Project::LongInteger pollard_p_minus_1_fact(const Project::LongInteger& n,
-                                            const Project::LongInteger& c,
-                                            const Project::LongInteger& max) {
-    Project::LongInteger m = c;
+LongInteger Pollard::pollard_p_minus_1_fact(const LongInteger& n, const LongInteger& c, const LongInteger& max) {
+    LongInteger m = c;
 
-    // EXPONENTIATE
-    for (Project::LongInteger i = 1; i < max; i++) {
-        // MODEXPO
-        m = UsefulAlgorithms::power_mod(m, i , n);
+    for (LongInteger i = 1; i < max; i++) {
+        m = Useful::power_mod(m, i , n);
         if (i % 10 == 0) {
-            // CHECK_GCD
-            Project::LongInteger g = UsefulAlgorithms::gcd(m - 1, n);
+            LongInteger g = Useful::gcd(m - 1, n);
             if (g == n) // we need to change c
                 return 0;
             else if (g > 1) // g is a proper divisor
@@ -23,4 +18,4 @@ Project::LongInteger pollard_p_minus_1_fact(const Project::LongInteger& n,
     }
     return 0; // we need to change c
 }
-}
+}  // namespace Project::Detail
--- PollardsAlgorithm/pollardAlg.h
+++ PollardsAlgorithm/pollardAlg.h
@@ -1,9 +1,8 @@
-#include "../project.h"
+#include "../integer.h"
 
-namespace Pollard {
-
-Project::LongInteger pollard_p_minus_1_fact(const Project::LongInteger& m,
-                                            const Project::LongInteger& c,
-                                            const Project::LongInteger& max);
-
-}
+namespace Project::Detail {
+class Pollard {
+public:
+    LongInteger pollard_p_minus_1_fact(const LongInteger& n, const LongInteger& c, const LongInteger& max);
+};
+}  // namespace Project::Detail
